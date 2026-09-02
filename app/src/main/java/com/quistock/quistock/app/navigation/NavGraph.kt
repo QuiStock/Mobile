00e3edd8d2df6b5cb7a121ad51982483ca766142
Bellac009 <bellacandido.camargo@gmail.com>
2026-09-02 20:35:44 -0300

    feat(navigation): add home destination and login-to-home route

--- app/src/main/java/com/quistock/quistock/app/navigation/NavGraph.kt
+++ app/src/main/java/com/quistock/quistock/app/navigation/NavGraph.kt
@@ -6,14 +6,14 @@ object NavGraph {
     val GRAPH_RES_ID = R.navigation.nav_graph
 
     object Destinations {
-        val LOGIN = R.id.loginFragment
-        val HOME = R.id.homeFragment
-        val REGISTER = R.id.cadastroPessoalFragment
+        const val LOGIN = R.id.loginFragment
+        const val REGISTER = R.id.cadastroPessoalFragment
+        const val HOME = R.id.homeFragment
     }
 
     object Actions {
-        val LOGIN_TO_REGISTER = R.id.action_loginFragment_to_cadastroPessoalFragment
-        val LOGIN_TO_HOME = R.id.action_loginFragment_to_homeFragment
-        val REGISTER_TO_LOGIN = R.id.action_cadastroPessoalFragment_to_loginFragment
+        const val LOGIN_TO_REGISTER = R.id.action_loginFragment_to_cadastroPessoalFragment
+        const val REGISTER_TO_LOGIN = R.id.action_cadastroPessoalFragment_to_loginFragment
+        const val LOGIN_TO_HOME = R.id.action_loginFragment_to_homeFragment
     }
 }
