d66c0aa7e336f2fd69c1df309e5b786c0c0e1cb2
Lucas Kluska Donini <lkd.lucas@gmail.com>
2026-09-17 11:24:36 -0300

    feat(chatbot): create chatbot use case

---
+++ app/src/main/java/com/quistock/quistock/domain/usecase/ChatbotUseCase.kt
@@ -0,0 +1,9 @@
+package com.quistock.quistock.domain.usecase
+
+import com.quistock.quistock.domain.model.ChatbotRequest
+import com.quistock.quistock.domain.model.ChatbotResponse
+import com.quistock.quistock.domain.port.ChatbotRepository
+
+class ChatbotUseCase(private val chatbotRepository: ChatbotRepository) {
+    suspend operator fun invoke(request: ChatbotRequest): ChatbotResponse = chatbotRepository.sendMessage(request)
+}
