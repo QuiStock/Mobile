2d458fa0628a421ab5523fcb0106a969708ed829
Lucas Kluska Donini <lkd.lucas@gmail.com>
2026-09-17 11:25:33 -0300

    fixup! feat(chatbot): create domain entities and port abstraction

--- app/src/main/java/com/quistock/quistock/domain/usecase/SendMessageToChatbotUseCase.kt
+++ app/src/main/java/com/quistock/quistock/domain/usecase/SendMessageToChatbotUseCase.kt
@@ -4,6 +4,6 @@ import com.quistock.quistock.domain.model.ChatbotRequest
 import com.quistock.quistock.domain.model.ChatbotResponse
 import com.quistock.quistock.domain.port.ChatbotRepository
 
-class ChatbotUseCase(private val chatbotRepository: ChatbotRepository) {
+class SendMessageToChatbotUseCase(private val chatbotRepository: ChatbotRepository) {
     suspend operator fun invoke(request: ChatbotRequest): ChatbotResponse = chatbotRepository.sendMessage(request)
 }
